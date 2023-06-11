a3600b4820faeb0de2645e82c56deb306ab44908
chenyv19980616 <chenyv19980616@163.com>
2023-06-11 13:30:19 +0800

    Initial commit

---
+++ boot3-demo01/src/test/java/com/atguigu/boot3demo01/Boot3Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot3demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot3Demo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
