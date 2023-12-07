15137700fed405982a6b662828d02975e8edf904
ChenYv <chenyv19980616@163.com>
2023-12-08 02:38:02 +0800

    mysql mybatis 集成完毕

--- boot3-demo01/src/main/java/com/atguigu/Boot3Demo01Application.java
+++ boot3-demo01/src/main/java/com/atguigu/Boot3Demo01Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Boot3Demo01Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Boot3Demo01Application.class, args);
     }
