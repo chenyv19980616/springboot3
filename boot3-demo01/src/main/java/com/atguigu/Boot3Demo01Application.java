495310b72f7f32aaefe7a162ea6fc4cd47ff2b38
ChenYv <chenyv19980616@163.com>
2023-12-08 01:24:32 +0800

    fix

--- boot3-demo01/src/main/java/com/atguigu/Boot3Demo01Application.java
+++ boot3-demo01/src/main/java/com/atguigu/Boot3Demo01Application.java
@@ -1,13 +1,1 @@
-package com.atguigu;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Boot3Demo01Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Boot3Demo01Application.class, args);
-    }
-
-}
+package com.atguigu;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class Boot3Demo01Application {    public static void main(String[] args) {        SpringApplication.run(Boot3Demo01Application.class, args);    }}
--- boot3-demo01/src/main/java/com/atguigu/controller/EmployeeController.java
+++ boot3-demo01/src/main/java/com/atguigu/controller/EmployeeController.java
@@ -1,26 +1,1 @@
-package com.atguigu.controller;
-
-import com.atguigu.dto.Employee;
-import com.atguigu.service.EmployeeService;
-import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @author : chenyv
- * @description : 表现层
- * @since : 2023-12-05 21:19
- */
-@RestController
-public class EmployeeController {
-    @Resource
-    private EmployeeService employeeService;
-
-    @PostMapping("/save")
-    public String save(@RequestBody Employee employee) {
-        Boolean isTrue = employeeService.saveToRedis(employee);
-        return isTrue ? "成功" : "失败";
-    }
-
-}
+package com.atguigu.controller;import com.atguigu.dto.Employee;import com.atguigu.service.EmployeeService;import jakarta.annotation.Resource;import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.RequestBody;import org.springframework.web.bind.annotation.RestController;/** * @author : chenyv * @description : 表现层 * @since : 2023-12-05 21:19 */@RestControllerpublic class EmployeeController {    @Resource    private EmployeeService employeeService;    @PostMapping("/save")    public String save(@RequestBody Employee employee) {        Boolean isTrue = employeeService.saveToRedis(employee);        return isTrue ? "成功" : "失败";    }}
