9136ae0e1e1e148b7786a6806116ffbd0390905f
ChenYv <chenyv19980616@163.com>
2023-12-25 22:56:54 +0800

    消息持久化

--- rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/three/Task02.java
+++ rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/three/Task02.java
@@ -2,6 +2,7 @@ package com.atguigu.rabbitmq.three;
 
 import com.atguigu.rabbitmq.utils.RabbitMqUtils;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
@@ -16,11 +17,12 @@ public class Task02 {
 
     public static void main(String[] args) throws Exception {
         Channel channel = RabbitMqUtils.getChannel();
-        channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
             String message = scanner.next();
-            channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            channel.basicPublish("", TASK_QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes(StandardCharsets.UTF_8));
             System.out.println("生产者发出消息：" + message);
         }
     }
