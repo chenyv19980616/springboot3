0e43f0b76d37f4d1e54176a15a22b62b38aee0c8
ChenYv <chenyv19980616@163.com>
2024-01-04 21:18:28 +0800

    处理未确认消息

--- rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/test04/ConfirmMessage.java
+++ rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/test04/ConfirmMessage.java
@@ -5,6 +5,8 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConfirmCallback;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * @author : chenyv
@@ -87,23 +89,40 @@ public class ConfirmMessage {
         String queueName = UUID.randomUUID().toString();
         //声明队列
         channel.queueDeclare(queueName, true, false, false, null);
-        long beginTime = System.currentTimeMillis();
+
+        /*
+        声明一个线程安全有序的hash表：outstandingConfirms
+        1。 轻松将序号与消息关联
+        2。 轻松删除消息
+        3。 支持高并发（多线程）
+         */
+        ConcurrentSkipListMap<Long, String> outstandingConfirmMap = new ConcurrentSkipListMap<>();
 
         //准备消息监听器
         ConfirmCallback ackCallback = (deliveryTag, multiple) -> {
-            //System.out.println("确认消息：" + deliveryTag);
-        };
-        ConfirmCallback nackCallback = (deliveryTag, multiple) -> {
-            //System.out.println("未确认消息：" + deliveryTag);
+            //删除已经确认的消息
+            ConcurrentNavigableMap<Long, String> confirmedMap = outstandingConfirmMap.headMap(deliveryTag);
+            confirmedMap.clear();
         };
 
+        ConfirmCallback nackCallback = (deliveryTag, multiple) -> {
+            String s = outstandingConfirmMap.get(deliveryTag);
+            System.out.println("未确认消息:" + deliveryTag + ":" + s);
+        };
+
+        //增加一个消息确认监听器
         channel.addConfirmListener(ackCallback, nackCallback);
+
+        //开始时间
+        long beginTime = System.currentTimeMillis();
 
         for (int i = 0; i < MESSAGE_COUNT; i++) {
             String message = i + "";
             channel.basicPublish("",queueName,null, message.getBytes());
+            //将需要发送的消息放入hash表中
+            outstandingConfirmMap.put(channel.getNextPublishSeqNo(), message);
         }
-
+        //结束时间
         long endTime = System.currentTimeMillis();
 
         System.out.println("异步确认，用时：" + (endTime - beginTime));
