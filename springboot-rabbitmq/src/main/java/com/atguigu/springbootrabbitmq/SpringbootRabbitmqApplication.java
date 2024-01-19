5931f8964aa0ca1cae9eda8d39aac0384d896e08
chenyv <cy01686806@cainiao.com>
2024-01-19 15:59:49 +0800

    延迟消息初版

--- springboot-rabbitmq/src/main/java/com/atguigu/springbootrabbitmq/SpringbootRabbitmqApplication.java
+++ springboot-rabbitmq/src/main/java/com/atguigu/springbootrabbitmq/SpringbootRabbitmqApplication.java
@@ -2,7 +2,9 @@ package com.atguigu.springbootrabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
+@EnableOpenApi
 @SpringBootApplication
 public class SpringbootRabbitmqApplication {
 
