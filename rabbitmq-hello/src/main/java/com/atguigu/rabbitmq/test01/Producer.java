18e789ad9a77e112d99e1888c6fa59bb287f3230
chenyv <cy01686806@cainiao.com>
2024-01-24 15:09:10 +0800

    优先级队列

--- rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/test01/Producer.java
+++ rabbitmq-hello/src/main/java/com/atguigu/rabbitmq/test01/Producer.java
@@ -1,8 +1,12 @@
 package com.atguigu.rabbitmq.test01;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author : chenyv
@@ -10,7 +14,7 @@ import com.rabbitmq.client.ConnectionFactory;
  * @since : 2023-12-13 22:18
  */
 public class Producer {
-    public static final String QUENE_NAME = "hello";
+    public static final String QUEUE_NAME = "hello";
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -20,11 +24,20 @@ public class Producer {
         factory.setPassword("wql19990322");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        channel.queueDeclare(QUENE_NAME, false, false, false, null);
 
-        //发消息
-        String message = "hello world !";
-        channel.basicPublish("", QUENE_NAME, null, message.getBytes());
-        System.err.println("发送完毕！");
+        Map<String, Object> arguments = new HashMap<>();
+        // 设置队列优先级范围为0-10
+        arguments.put("x-max-priority", 10);
+        channel.queueDeclare(QUEUE_NAME, true, false, false, arguments);
+
+
+        for (int i = 1; i <11 ; i++) {
+            String message = "info" + i;
+            AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                    .priority(i)
+                    .build();
+            channel.basicPublish("", QUEUE_NAME, properties, message.getBytes());
+
+        }
     }
 }
