a1cb0a09108791489f05e86c3c022fbda680f950
ChenYv <chenyv19980616@163.com>
2024-02-15 11:50:05 +0800

    SpringBoot3 yaml语法

--- boot3-demo01/src/main/java/com/atguigu/Boot3Demo01Application.java
+++ boot3-demo01/src/main/java/com/atguigu/Boot3Demo01Application.java
@@ -1,5 +1,6 @@
 package com.atguigu;
 
+import com.atguigu.dto.Person;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.atguigu.mapper")
 public class Boot3Demo01Application {
     public static void main(String[] args) {
-        SpringApplication.run(Boot3Demo01Application.class, args);
+        var ioc = SpringApplication.run(Boot3Demo01Application.class, args);
+        Person person = ioc.getBean(Person.class);
+        System.err.println("person = " + person);
     }
 
 }
