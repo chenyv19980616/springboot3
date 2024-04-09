3ffe496c5d4b0b187a09c53ac9a7aafd4583010f
chenyv <cy01686806@cainiao.com>
2024-04-09 12:45:16 +0800

    lock 实现卖票

--- JUC/src/com/atguigu/threadsafe01/MyThread.java
+++ JUC/src/com/atguigu/threadsafe01/MyThread.java
@@ -1,5 +1,8 @@
 package com.atguigu.threadsafe01;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author : chenyv
  * @since : 2024-04-08 21:54
@@ -8,23 +11,24 @@ public class MyThread extends Thread {
 
     static int tickets = 0;
 
+    static Lock lock = new ReentrantLock();
+
     @Override
     public void run() {
         while (true) {
-            // 同步代码块
-            synchronized (MyThread.class) {
+            lock.lock();
+            try {
                 if (tickets < 100) {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
+                    Thread.sleep(10);
                     tickets++;
                     System.out.println(getName() + "正在卖第" + tickets + "张票！");
                 } else {
                     break;
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
             }
         }
     }
