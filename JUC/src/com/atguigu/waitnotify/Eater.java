40bef80c8111ce6f5d0ea5fdc61476c47113504a
ChenYv <chenyv19980616@163.com>
2024-04-11 21:02:10 +0800

    吃饭demo  吃货代码

--- JUC/src/com/atguigu/waitnotify/Eater.java
+++ JUC/src/com/atguigu/waitnotify/Eater.java
@@ -9,17 +9,20 @@ public class Eater extends Thread {
     public void run() {
         while (true) {
             synchronized (Desk.lock) {
-                if (Desk.foodFlag == 0) {
-                    try {
-                        Desk.lock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                if (Desk.count == 0) {
+                    break;
                 } else {
-                    System.out.println("吃饭中，还能再吃" + Desk.count + "碗");
-                    Desk.lock.notifyAll();
-                    Desk.count--;
-                    Desk.foodFlag = 0;
+                    if (Desk.foodFlag == 0) {
+                        try {
+                            Desk.lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        System.out.println("吃货正在吃面条，还能再吃" + --Desk.count + "碗");
+                        Desk.foodFlag = 0;
+                        Desk.lock.notifyAll();
+                    }
                 }
             }
         }
